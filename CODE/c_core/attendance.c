025f20639dd2482ad93b9d1da9024d9bfc2ab86d
Abhinav08bhatt <abhinav08bhatt@gmail.com>
2026-03-25 18:42:30 +0530

    WIP : implemented menu

--- CODE/c_core/attendance.c
+++ CODE/c_core/attendance.c
@@ -1,23 +1,7 @@
 #include <stdio.h>
 
 void markAttendance(char name[], int present_num){
-    FILE *attendance_data_file_pointer;
 
-    attendance_data_file_pointer = fopen("attendance_data.txt","r");
-
-    char attendance_data_instance[100000];
-    while(fgets(attendance_data_instance,100000,attendance_data_file_pointer)!=NULL){
-        printf("%s",attendance_data_instance);
-    }
-
-    printf("Marked attendance of the student");
     
 
-    // change : (test)
-    printf("I am making changes");
-
-    // change 2
-    printf("hi");
-
-    // made changes
 }
--- CODE/c_core/menu.c
+++ CODE/c_core/menu.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
+#include <string.h>
 
 #include "student.c"
 #include "attendance.c"
 
+void showMenu(){
+    printf("\n+=================================================================+");
+    printf("\n|======  Enter the valid input number for respective action ======|");
+    printf("\n+=================================================================+");
+    printf("\n|  ( 1 )   ADD a new student                                      |");
+    printf("\n|  ( 2 )   REMOVE an old student                                  |");
+    printf("\n+=================================================================+");
+    printf("\n");
+}
+
 int main(){
 
-    char name_of_student[10] = {'A','V','I'};
-    
-    createStudent(name_of_student);
-    
+    showMenu();
+
     return 0;
 }
